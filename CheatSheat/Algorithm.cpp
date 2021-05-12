1206ac99dc3a11281dee95938c18265d94cffdd6
Yorosu-10 <jump52433777swaan@gmail.com>
2021-05-12 18:51:13 +0900

    チートシートを整理

---
+++ CheatSheat/Algorithm.cpp
@@ -0,0 +1,26 @@
+//バケット法
+// ABC200C
+
+// ビット全探索
+// ABC45C
+// ABC128C
+// ABC147C
+// ABC79C
+for (int bit = 0; bit < (1 << N); ++bit)
+{
+}
+
+// 順列全探索
+// ABC145C
+// ABC150C
+vector<int> ord(N);
+for (int i = 0; i < N; i++)
+{
+    ord[i] = i;
+}
+
+do
+{
+
+} while (next_permutation(ord.begin(), ord.end()));
+
---
+++ CheatSheat/Template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
